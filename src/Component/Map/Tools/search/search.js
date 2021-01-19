05d7c442e07c4ecc44bd78e49c9f603b06e79091
mohab <homab3@gmail.com>
2021-01-19 16:25:37 +0200

    fixing a search box position

--- src/Component/Map/Tools/search/search.js
+++ src/Component/Map/Tools/search/search.js
@@ -6,25 +6,23 @@ export default function Search() {
     
     const [SearchBarDisplay,toggleSearchBar] = useState('none')
     const [SearchBoxColor,toggleSearchBoxColor] = useState('orange')
-    const [compensationValue,toggleCompensationValue] = useState(0) // this because other elements affects the position of the icon when they are shown on the screen.
     
     const handleClick=(e)=>{
         SearchBoxColor =='orange'? toggleSearchBoxColor('orangered'):toggleSearchBoxColor('orange')
-        SearchBarDisplay==='none'?toggleSearchBar('block'):toggleSearchBar('none')
-        compensationValue===0?toggleCompensationValue(-12):toggleCompensationValue(0)
+        SearchBarDisplay=='none'?toggleSearchBar('block'):toggleSearchBar('none')
     }
     
     return (
-        <div className='search' onClick={handleClick} style={{backgroundColor:SearchBoxColor}}>
+        <div className='search' style={{backgroundColor:SearchBoxColor}}>
             <div className='search-container'>
-                <div className='search-box'>
-                    <div style={{marginTop:compensationValue}}>
+                <div className='search-box' onClick={handleClick}>
+                    <div>
                         <i className='fa fa-search'>
                         </i>
                     </div>
                 </div>
-                <SearchingComponent SearchBarDisplay={SearchBarDisplay}/>
             </div>
+            <SearchingComponent SearchBarDisplay={SearchBarDisplay}/>
         </div>
     )
 }
