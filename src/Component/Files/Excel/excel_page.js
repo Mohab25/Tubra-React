fac541cd556421c45e9ea31ddad762bf173007bb
mohab <homab3@gmail.com>
2021-03-03 10:55:24 +0200

    completing Excel reader MVP

--- src/Component/Files/Excel/excel_page.js
+++ src/Component/Files/Excel/excel_page.js
@@ -9,20 +9,36 @@ export default function Excel_page(props) {
   let {content} = props
   let content_ob = JSON.parse(content)
   let data=[]
-  //console.log(content_ob['chainage'])
-  for(let i of Object.keys(content_ob['chainage'])){
-    data.push({'chainage':content_ob['chainage'][i]})
-  }
+  // for(let i of Object.keys(content_ob)){
+  //   for(let j of Object.keys(content_ob[i])){
+  //     let ob={}
+  //     ob[i] = content_ob[i][j]
+  //     // get to the second key, grab the first value and add them to the object.
+  //     // continue doing this until you reached the end of keys, then go to the next 
+  //     // value... 
+  //     data.push(ob)
+  //   }
+    
+  // }
 
+  for(let m of Object.keys(content_ob[Object.keys(content_ob)[0]])){
+    let ob = {}
+    for(let i=0;i<Object.keys(content_ob).length;i++){
+      for(let j=0;j< Object.keys(content_ob[Object.keys(content_ob)[0]]).length;j++){
+        ob[Object.keys(content_ob)[i]] = content_ob[Object.keys(content_ob)[i]][m]
+        break
+        // here the benefit of j that it ensures only one value will be return and then break.
+      }
+      
+    }
+    data.push(ob)
+  }
   let columns = []
   for(let i of Object.keys(content_ob)){
     let ob = {title:i,key:i}
     columns.push(textColumn(ob))
-    break 
   }
 
-  console.log(data)    
-  console.log(columns)    
     return (
         <div className='Excel-sheet'>
           <div className='Excel-sheet-container'>
