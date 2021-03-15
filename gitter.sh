2ba43669b9f34c452b29ddc1a82414747b563d01
mohab <homab3@gmail.com>
2021-03-15 20:08:54 +0200

    remove token from automating script and comment

--- gitter.sh
+++ gitter.sh
@@ -7,10 +7,10 @@ git push origin main
 git checkout master
 git branch -d main
 echo "commit end successfully !"
-echo "input pull message title"
-read title
-echo "input pull comment"
-read comment
-$ curl -v -u Mohab25:cf5596a1fd293eba2ba3dde38497c2333ecd97f3 -H "Content-Type:application/json" -X POST https://api.github.com/repos/Mohab25/Tubra-React/pulls -d '{"title":'$title', "body":'$comment', "head": "main", "base": "master"}'
+#echo "input pull message title"
+#read title
+#echo "input pull comment"
+#read comment
+#$ curl -v -u Mohab25: -H "Content-Type:application/json" -X POST https://api.github.com/repos/Mohab25/Tubra-React/pulls -d #'{"title":'$title', "body":'$comment', "head": "main", "base": "master"}'
 
-echo "end making pull request"
+#echo "end making pull request"
