2eab6c367229143aece2c99e64ccde5c7e1cf7e8
mohab <homab3@gmail.com>
2021-03-24 19:14:13 +0200

    Completing first Dashboard Elements

--- src/Component/D3Circle/D3Circle.js
+++ src/Component/D3Circle/D3Circle.js
@@ -1,30 +1,30 @@
 import React,{useEffect,useState} from 'react'
 import './styles/styles.css'
-export default function D3Circle({id_name,width,height,x,y,mainText,subText,mainTextColor,subTextColor,innerCircleColor,outerCircleDisplay,outerCircleColor}) {
-
-let [w,setW] = useState(width==undefined?'500':width); let [h,setH] = useState(height==undefined?'500':height); 
+export default function D3Circle(props) {
+console.log(props)
+let [w,setW] = useState(props.width==undefined?'500':props.width); let [h,setH] = useState(props.height==undefined?'500':props.height); 
 
 
 useEffect(()=>{
-    let svg = window.d3.select(`#${id_name}`).append('svg').attr('width',w).attr('height',h)
-    let circleGroup= svg.append('g').attr('transform',`translate(${x==undefined?w/2:x} ${y==undefined?h/2:y})`)
-    let textGroup= svg.append('g').attr('transform',`translate(${x==undefined?w/2:x} ${y==undefined?h/2:y})`)
-    let circle = circleGroup.append('circle').attr('r',100).attr('fill','none').attr('stroke',innerCircleColor).attr('stroke-width','3.5')
-    let text = textGroup.append('text').text(mainText).attr('fill',mainTextColor).attr('dx','-1.2em').attr('dy','0').attr('class','circle-text') 
-    let text2 = textGroup.append('text').text(subText).attr('fill',subTextColor).attr('dx','-1.7em').attr('dy','30').attr('class','circle-text-2') 
+    let svg = window.d3.select(`#${props.id_name}`).append('svg').attr('width',w).attr('height',h)
+    let circleGroup= svg.append('g').attr('transform',`translate(${props.circle_x==undefined?w/2:props.circle_xx} ${props.circle_y==undefined?h/2:props.circle_y})`)
+    let textGroup= svg.append('g').attr('transform',`translate(${props.text_x==undefined?w/2:props.text_x} ${props.text_y==undefined?h/2:props.text_y})`)
+    let circle = circleGroup.append('circle').attr('r',props.innerCircleRadius).attr('fill','none').attr('stroke',props.innerCircleColor).attr('stroke-width','3.5')
+    let text = textGroup.append('text').text(props.mainText).attr('fill',props.mainTextColor).attr('dx',props.MainTextPositionDelta.dx).attr('dy',props.MainTextPositionDelta.dy).attr('font-size',props.MainTextSize).attr('class','circle-text') 
+    let text2 = textGroup.append('text').text(props.subText).attr('fill',props.subTextColor).attr('dx',props.subTextPositionDelta.dx).attr('dy',props.subTextPositionDelta.dy).attr('font-size',props.subTextSize).attr('class','circle-text-2') 
     
     
-    let arcGroup = svg.append('g').attr('transform',`translate(${x==undefined?w/2:x} ${y==undefined?h/2:y})`)
-    arcGroup.append('path').attr('d',window.d3.arc().innerRadius( 100 )
-    .outerRadius( 120 )
+    let arcGroup = svg.append('g').attr('transform',`translate(${props.circle_x==undefined?w/2:props.circle_x} ${props.circle_y==undefined?h/2:props.props.circle_y})`)
+    arcGroup.append('path').attr('d',window.d3.arc().innerRadius( props.outerCircleInnerRadius)
+    .outerRadius( props.outerCircleOuterRadius )
     .startAngle(0)     // It's in radian, so Pi = 3.14 = bottom.
     .endAngle(0.78*Math.PI )       // if you begin with 0, end with Math.PI will give half a circle
-    ).attr('fill',outerCircleColor).attr('display',outerCircleDisplay)
+    ).attr('fill',props.outerCircleColor).attr('display',props.outerCircleDisplay)
     
 },[])
 
     return (
-        <div id={id_name} style={{width:w,height:height}}>
+        <div id={props.id_name} style={{width:props.w,height:props.height}}>
             
         </div>
     )
@@ -38,5 +38,12 @@ D3Circle.defaultProps={
     subTextColor:'red',
     innerCircleColor:'gainsboro',
     outerCircleDisplay:'', // this wll display the outer circle
-    outerCircleColor:'red'
+    outerCircleColor:'red',
+    innerCircleRadius:100,
+    outerCircleInnerRadius:100,
+    outerCircleOuterRadius:120,
+    MainTextPositionDelta:{dx:'-1.2em',dy:"0"},
+    subTextPositionDelta:{dx:'-1.7em',dy:"1.5em"},
+    MainTextSize:'3em',
+    subTextSize:''
 }
--- src/Component/Dashboard/index.js
+++ src/Component/Dashboard/index.js
@@ -11,11 +11,25 @@ export default function Dashboard() {
             <div className='Dashboard-container' style={{width:'100%',height:'100%'}}>
                 <div className='ganttChart-container'></div>
                 <div className='Dashboard-pieCharts-container'>
-                    <PieChart id_name='c1' width='300' height='300'/>
-                    <PieChart id_name='c2' width='300' height='300'/>
-                </div>
+                    <div className='Dashboard-PieChart-card-container'>
+                        <h4>Overall progress</h4>
+                        <PieChart id_name='c1' width='180' height='180' MainTextSize='1.5em' subTextSize={'.5em'} innerCircleRadius={80} outerCircleInnerRadius={80} outerCircleOuterRadius={90} mainTextColor='green' subTextColor='green' outerCircleColor='green'/>
+                        <div className='Dashboard-PieChart-card-content'>
+                            <div><i className='fa fa-bell' style={{color:'green'}}></i><p>Completed <b>9</b></p></div>
+                            <div><i className='fa fa-pen'  style={{color:'orange'}}></i><p>DoorDash <b>12</b></p></div>
+                        </div>
+                    </div>
+                    <div className='Dashboard-PieChart-card-container'>
+                        <h4>Overall Delay</h4>
+                        <PieChart id_name='c2' width='180' height='180' MainTextSize='1.5em' subTextSize={'.5em'} innerCircleRadius={80} outerCircleInnerRadius={80} outerCircleOuterRadius={90}/>
+                        <div className='Dashboard-PieChart-card-content'>
+                            <div><i className='fa fa-bell' style={{color:'green'}}></i><p>Completed <b>9</b></p></div>
+                            <div><i className='fa fa-pen'  style={{color:'orange'}}></i><p>DoorDash <b>12</b></p></div>
+                        </div>
+                    </div>
             </div>
         </div>
+    </div>
     )
 }
 
