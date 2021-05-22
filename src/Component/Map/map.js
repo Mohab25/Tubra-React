1204a644e9016c634ce421602f7a7f1e897e73c9
mohab <homab3@gmail.com>
2021-05-22 19:43:43 +0200

    cleaning up map component

--- src/Component/Map/map.js
+++ src/Component/Map/map.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import {renderToStaticMarkup} from 'react-dom/server'
 import {useState,useEffect,useRef} from 'react'
 import {useSelector,useDispatch} from 'react-redux'
 import './styles/styles.css'
-import {Map,TileLayer,GeoJSON,LayersControl,LayerGroup,Polyline,Polygon,Marker} from 'react-leaflet'
+import {Map,TileLayer,LayersControl,LayerGroup} from 'react-leaflet'
 import './leaflet/leaflet.css'
-import L from "leaflet";
 import MapToolsHolder from './MapToolsHolder/MapToolsHolder'
-import {divIcon } from 'leaflet'
 import PavementConstructionGeojson from './GeojsonComponents/PavementConstructionGeojson'
 import AerodromeEntityGeoJSON from './GeojsonComponents/AerodromeGeojsonComponent'
 import Modal from './Modal/modal'
 import './MakerIcon/styles/styles.css'
-import Legend from './Legend/Legend'
 import toggleGeometryCreationFormVisibility from '../../Actions/GeometryCreation/ShowHideCreationForm'
 import prePopulateGeometry from '../../Actions/GeometryCreation/GeometryCreation'
 import GeometryCreationModal from './Tools/Vector Geometry/GeometryCreationModal/GeometryCreationModal'
@@ -47,14 +43,6 @@ export default function MapComponent() {
     /* Buffer creation */
     const [isBufferActivated,setBufferActive]= useState(false)
     const bufferActivation = useSelector(state=>state.bufferReducer.isBufferToolActivated)
-    const dispatchedBufferDistance  = useSelector(state=>state.BufferAddRemoveReducer.distance)
-    const createBufferDispatch = useDispatch()
-    /* filtered points for custom markers */ 
-    const [PointsMarkers,setPointsMarkers] = useState([])
-    const [Markers,setMarkers] = useState([])
-
-    /*Legend */
-    const [legend,setLegendNames]=useState([])
 
     /* Geometry Creation */
     const GeometryActionDispatch = useDispatch()
@@ -115,78 +103,37 @@ export default function MapComponent() {
 
     }
 
-    let createPoint=(e)=>{
-        /* what is need to create a point geometry is to get the coords, and send them
-            to the backend, where the geometry will be created.  
-        */
+    // let createPoint=(e)=>{
+    //     /* what is need to create a point geometry is to get the coords, and send them
+    //         to the backend, where the geometry will be created.  
+    //     */
         
-        /*
-            who to redraw GeoJSON:
+    //     /*
+    //         who to redraw GeoJSON:
 
-        */
-            // calling redux to open the Modal. 
-            GeometryActionDispatch(toggleGeometryCreationFormVisibility())
-            // you need to get the data from creation form... it goes as useSelector and sense the change on redux store, update the state and use the updated state to complete the geometry creation.  
-            let json_point=''
-            if(PointsMarkers.length!==0){
-            //spread the PointsMarkers array and add a jon point to it.  
-            let points = [...PointsMarkers]
-            json_point = { "type": "Feature", "properties": {'Feature_Name':'Random Point'}, "geometry": { "type": "Point", "coordinates":[e.latlng.lng,e.latlng.lat]}}           
-            points.push(json_point)
-            setPointsMarkers(points)
-            }
+    //     */
+    //         // calling redux to open the Modal. 
+    //         GeometryActionDispatch(toggleGeometryCreationFormVisibility())
+    //         // you need to get the data from creation form... it goes as useSelector and sense the change on redux store, update the state and use the updated state to complete the geometry creation.  
+    //         let json_point=''
+    //         if(PointsMarkers.length!==0){
+    //         //spread the PointsMarkers array and add a jon point to it.  
+    //         let points = [...PointsMarkers]
+    //         json_point = { "type": "Feature", "properties": {'Feature_Name':'Random Point'}, "geometry": { "type": "Point", "coordinates":[e.latlng.lng,e.latlng.lat]}}           
+    //         points.push(json_point)
+    //         setPointsMarkers(points)
+    //         }
     
-            // also because the geometry is not defined in the form, it needs to be dispatched from here
-            GeometryDispatch(prePopulateGeometry(json_point))
+    //         // also because the geometry is not defined in the form, it needs to be dispatched from here
+    //         GeometryDispatch(prePopulateGeometry(json_point))
 
-        }
-
-/***********************Custom Marker Filtering *******************************/
-
-useEffect(()=>{
-    if(PointsMarkers.length==0){return}
-    let points_icons = PointsMarkers.map((item,index)=>{
-        return(
-            renderToStaticMarkup(        
-                <div className='Maker-icon'>
-                <div className='Marker-icon-container'>
-                    <p>{item.properties.Feature_Name.slice(0,1)}</p>
-                </div>
-                </div>)
-        )
-    })
-    let icons = points_icons.map((item,index)=>{
-        return divIcon({
-                html: item,
-          });
-    })
-    
-    let markers = icons.map((item,index)=>{
-            let pos = PointsMarkers[index].geometry.coordinates
-            return <Marker icon={item} position={[pos[1],pos[0]]} key={Math.random()}/>
-    })
-    setMarkers(markers)
-
-    //also you can account for the legend names here, as the legend names should be unique
-    let legend_names = PointsMarkers.map(item=>item.properties.Feature_Name)
-    let unique_names = legend_names.filter((value,index,self)=>{
-        return self.indexOf(value) === index;
-    })
-    let Legend_names_object = {}
-    unique_names.map(item=>{
-        Legend_names_object[item.slice(0,1)]=item
-    })
-    setLegendNames(Legend_names_object)
-
-
-},[PointsMarkers])
-
+    //     }
 
 /****************************** Rendering  *********************************/
     return (
         <div className='Map-outer-container'>
             <div className='Map-container'>
-                <Map className='Map' center={mapCenter} zoom={zoom} ref={mapRef} onclick={isBufferActivated?createPoint:makeLinearMeasurement}>
+                <Map className='Map' center={mapCenter} zoom={zoom} ref={mapRef} onclick={isBufferActivated?"":makeLinearMeasurement}>
                     <LayersControl position='topleft' className='layers-control'>
                         <TileLayer url={tile}/>
                             <Overlay name='pavement construction'>
@@ -204,8 +151,8 @@ useEffect(()=>{
                     {entityModalData!=null && <Modal data={entityModalData} modalCloser={setEntityModalData}/>}
                     {pavementModalData!=null && <Modal data={pavementModalData} modalCloser={setPavementModalData}/>}
                 </Map>
-                <MapToolsHolder toggleLinearMeasurement={toggleLinearMeasurement} distance={Calculated_distance} activateVector={activateVector}  createPoint={createPoint}/>
-                <Legend legendItems={Object.keys(legend).length==0?"":legend}/>
+                <MapToolsHolder toggleLinearMeasurement={toggleLinearMeasurement} distance={Calculated_distance} activateVector={activateVector} />
+                
                 <GeometryCreationModal/>
             </div>
         </div>
