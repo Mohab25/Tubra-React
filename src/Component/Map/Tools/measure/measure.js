bb944989d4c421aceefc0a2602900cbd34e5fbda
mohab <homab3@gmail.com>
2021-06-16 19:21:05 +0200

    adding to measure tool

--- src/Component/Map/Tools/measure/measure.js
+++ src/Component/Map/Tools/measure/measure.js
@@ -1,8 +1,9 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import { useSelector } from "react-redux";
 import './styles/styles.css'
 import LinearMeasurePane from './LinearMeasurePane'
 import Tooltip from '../../../Tooltips/Tooltips'
+import L from "leaflet";
 //geoman
 import '@geoman-io/leaflet-geoman-free';  
 import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';  
@@ -18,6 +19,13 @@ export default function Measure(props) {
     // getting the map 
     let map = useSelector(state=>state.gettingMapRefReducer.MapRef)
 
+    // measure pair
+    const [first_latlng,set_first_latlng] = useState([])
+    const [sec_latlng,set_sec_latlng] = useState([])
+
+    const [distance,set_distance] = useState(0)
+    const [polylines,add_polylines] = useState([])
+
     const toggleMeasureToolColor=()=>{
         MeasureToolColor=='orange'?setMeasureToolColor('orangered'):setMeasureToolColor('orange')
     }
@@ -26,30 +34,71 @@ export default function Measure(props) {
         LinearMeasurePaneDisplay=='none'?setLinearMeasurePaneDisplay('block'):setLinearMeasurePaneDisplay('none')
     }
 
-    const lineRulerCreation=(e)=>{
-        console.log('the e',e)
-        if(LinearMeasurePaneDisplay=='none'){
-            
-            map.pm.disableDraw()
-            // open draw mode     
-            map.pm.enableDraw('Line', {
-            snappable: false,
-            snapDistance: 20,
-            continueDrawing:true,
-            });  
-        }
 
-        else{
-            map.pm.disableDraw()
+    if(LinearMeasurePaneDisplay!='none'){
+        map.off().on('click',(e)=>{
+        console.log('map clicked');
+        if(first_latlng.length==0&&sec_latlng.length==0){
+            set_first_latlng(e.latlng)
         }
-        
-        
+        else if(first_latlng.length!=0&&sec_latlng.length==0){
+            set_sec_latlng(e.latlng)
+            measure(1,e.latlng)
+        }
+        else if(first_latlng.length==0&&sec_latlng.length!=0){
+            set_first_latlng(e.latlng)
+            measure(2,e.latlng)
+        }
+    
+    })}
+
+
+    const measure =(i,m)=>{
+        let l1={},l2={}
+        if(i==1){
+            l1 = L.latLng(first_latlng.lat,first_latlng.lng)
+            l2 = L.latLng(m.lat,m.lng)
+            set_first_latlng([])
+        }
+        else{
+            l1 = L.latLng(m.lat,m.lng)
+            l2 = L.latLng(sec_latlng.lat,sec_latlng.lng)
+            set_sec_latlng([])
+        }
+        draw(l1,l2)
     }
 
+    let draw=(l1,l2)=>{
+        let d = l1.distanceTo(l2)
+        let poly = [l1,l2]
+        let polyline = new L.Polyline(poly)
+        polyline.addTo(map)
+        let poly_set=polylines
+        poly_set.push(polyline)
+        add_polylines(poly_set)
+        set_distance(d)
+    }
+
+    let remove_polylines=()=>{
+        for(let p of polylines){
+            map.removeLayer(p)
+            set_first_latlng([])
+            set_sec_latlng([])
+            set_distance(0)
+        }
+    }
+
+    useEffect(()=>{
+        if(LinearMeasurePaneDisplay=='none'){
+            map.off();
+            remove_polylines()
+            
+        }
+    },[LinearMeasurePaneDisplay])
 
     return (
         <>
-        <div className='measure' onClick={()=>{toggleMeasureToolColor();toggleLinearMeasurePaneDisplay();props.toggleLinearMeasurement();lineRulerCreation()}} style={{backgroundColor:MeasureToolColor}} onMouseEnter={()=>toggleTooltipDisplay('flex')} onMouseLeave={()=>{toggleTooltipDisplay('none')}}>
+        <div className='measure' onClick={()=>{toggleMeasureToolColor();toggleLinearMeasurePaneDisplay();props.toggleLinearMeasurement()}} style={{backgroundColor:MeasureToolColor}} onMouseEnter={()=>toggleTooltipDisplay('flex')} onMouseLeave={()=>{toggleTooltipDisplay('none')}}>
             <div className='measure-container'>
                 <div className='measure-box'>
                     <div>
@@ -58,7 +107,7 @@ export default function Measure(props) {
                 </div>
             </div>
         </div>
-        <LinearMeasurePane display={LinearMeasurePaneDisplay} distance={props.distance}/>
+        <LinearMeasurePane display={LinearMeasurePaneDisplay} distance={distance}/>
         <Tooltip display={toolTipDisplay} name='measure' toolIndex={3}/>
         </>
     )
--- src/Component/Map/map.js
+++ src/Component/Map/map.js
@@ -30,8 +30,9 @@ export default function MapComponent() {
     const [Calculated_distance,setCalculatedDistance] = useState(0)
     /* Linear Measurements */
     const [linear_measure_is_on,toggle_linear_measure] = useState(false)
-    const [linear_coords,setLinearCoords]=useState([])
+    //const [linear_coords,setLinearCoords]=useState([])
     const linearMeasureDispatch = useDispatch()
+
 
 
     /* Buffer creation */
@@ -49,27 +50,27 @@ export default function MapComponent() {
         linear_measure_is_on==false?toggle_linear_measure(true):toggle_linear_measure(false)
     }
 
-    const makeLinearMeasurement=(e)=>{
-        if(linear_measure_is_on){
-            if(linear_coords.length==0){
-                // the decimal degrees from leaflet comes with 14 decimal places, limit it to 6
-                let first_pair = [parseFloat(e.latlng.lat.toFixed(6)),parseFloat(e.latlng.lng.toFixed(6))]
-                setLinearCoords(first_pair)
-            }
-            else{
-                let first_pairs = linear_coords
-                let second_pairs = [parseFloat(e.latlng.lat.toFixed(6)),parseFloat(e.latlng.lng.toFixed(6))]
-                fetch('http://localhost:8000/spatial_analysis/linear_measure/',{
-                    method:'POST',
-                    mode:'cors',
-                    headers:{
-                        'Content-Type':'application/json'
-                    },
-                    body:JSON.stringify({coord1:first_pairs,coord2:second_pairs})
-            }).then(res=>res.json()).then(data=>setCalculatedDistance(data*111.32*1000))
-            setLinearCoords([])
-    }
-    }}
+    // const makeLinearMeasurement=(e)=>{
+    //     if(linear_measure_is_on){
+    //         if(linear_coords.length==0){
+    //             // the decimal degrees from leaflet comes with 14 decimal places, limit it to 6
+    //             let first_pair = [parseFloat(e.latlng.lat.toFixed(6)),parseFloat(e.latlng.lng.toFixed(6))]
+    //             setLinearCoords(first_pair)
+    //         }
+    //         else{
+    //             let first_pairs = linear_coords
+    //             let second_pairs = [parseFloat(e.latlng.lat.toFixed(6)),parseFloat(e.latlng.lng.toFixed(6))]
+    //             fetch('http://localhost:8000/spatial_analysis/linear_measure/',{
+    //                 method:'POST',
+    //                 mode:'cors',
+    //                 headers:{
+    //                     'Content-Type':'application/json'
+    //                 },
+    //                 body:JSON.stringify({coord1:first_pairs,coord2:second_pairs})
+    //         }).then(res=>res.json()).then(data=>setCalculatedDistance(data*111.32*1000))
+    //         setLinearCoords([])
+    // }
+    // }}
 
 /**************************Buffer**************************************************/
 
@@ -111,7 +112,7 @@ export default function MapComponent() {
     return (
         <div className='Map-outer-container'>
             <div className='Map-container'>
-                <Map className='Map' center={mapCenter} zoom={zoom} ref={mapRef} onclick={isBufferActivated?createBuffer:makeLinearMeasurement}>
+                <Map className='Map' center={mapCenter} zoom={zoom} ref={mapRef} onclick={isBufferActivated?createBuffer:null}>
                     <LayersControl position='topleft' className='layers-control'>
                         <TileLayer url={tile}/>
                             <Overlay name='pavement construction'>
@@ -127,7 +128,7 @@ export default function MapComponent() {
                         <BufferComponent/>  
                     </LayersControl>
                 </Map>
-                <MapToolsHolder toggleLinearMeasurement={toggleLinearMeasurement} distance={Calculated_distance} activateVector={activateVector} />
+                <MapToolsHolder toggleLinearMeasurement={toggleLinearMeasurement}  activateVector={activateVector} />
                 
                 <SearchingComponent/>
                 <GeometryCreationModal/>
